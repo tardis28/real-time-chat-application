2f7b5e0dd38795ccd9e9884633e8d45769a89af0
Shreya Sneha <shreyasneha29@gmail.com>
2020-11-22 14:32:30 +0530

    established connection

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ const http = require('http')
 const express = require('express')
 const socketio =  require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if (error) {
+            return callback(error)
+        }
